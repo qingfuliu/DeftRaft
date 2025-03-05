50689b2b62ab63e7daf711ecaec3e4f8fde82842
calyqfliu <calyqfliu@tencent.com>
2025-03-05 19:15:45 +0800

    add protobuf

---
+++ src/include/storage/Storage.h
@@ -0,0 +1,18 @@
+//
+// Created by root on 2/21/25.
+//
+
+#ifndef DEFTRAFT_STORAGE_H
+#define DEFTRAFT_STORAGE_H
+
+namespace CLSN {
+
+
+    class Storage {
+    public:
+
+    };
+
+} // CLSN
+
+#endif //DEFTRAFT_STORAGE_H
